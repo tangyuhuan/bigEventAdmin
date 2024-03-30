ed246b558258ce6fb11f1d80a5dc3f78a544d497
yuhuan <597270222@qq.com>
2024-03-30 23:41:03 +0800

    test

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    //prettier专注于代码美观度，是一个格式化工具
+    //前置：
+    //1.禁用格式化插件 prettier format on save关闭
+    //2.安装Eslint插件，并配置保存时自动修复
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true, // 单引号
+        semi: false, // 无分号
+        printWidth: 80, // 每行宽度至多80字符
+        trailingComma: 'none', // 不加对象|数组最后逗号
+        endOfLine: 'auto' // 换行符号不限制（win mac 不一致）
+      }
+    ],
+    //ESlint关注于规范，如果不符合规范会报错
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+      }
+    ],
+    'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
+    // 💡 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
+    'no-undef': 'error'
+  }
+}
