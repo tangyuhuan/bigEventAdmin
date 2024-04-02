121c79a021b0a04b836a2986bfcf5a3e8eacb15c
yuhuan <597270222@qq.com>
2024-04-02 10:26:12 +0800

    注册登录 静态结构 & 基本切换

--- src/App.vue
+++ src/App.vue
@@ -4,35 +4,35 @@
 // const router = useRouter()
 //2.获取路由参数 route 有专门的方法useRoute
 // const route = useRoute()
-import { useRoute, useRouter } from 'vue-router'
+// import { useRoute, useRouter } from 'vue-router'
 // import { useUserStore } from '@/stores/modules/user'
-import { useUserStore } from '@/stores'
+// import { useUserStore } from '@/stores'
 //导入，就当普通axios来用
-import axios from '@/utils/request'
-const userStore = useUserStore()
-const router = useRouter()
-const route = useRoute()
+// import axios from '@/utils/request'
+// const userStore = useUserStore()
+// const router = useRouter()
+// const route = useRoute()
 
-const goList = () => {
-  router.push('/list')
-  console.log(router) //路由对象
-  console.log(route) //路由参数
-}
-async function getUser() {
-  const response = await axios.get('/my/cate/list?id=1284')
-  console.log(response)
-}
+// const goList = () => {
+//   router.push('/list')
+//   console.log(router) //路由对象
+//   console.log(route) //路由参数
+// }
+// async function getUser() {
+//   const response = await axios.get('/my/cate/list?id=1284')
+//   console.log(response)
+// }
 </script>
 <template>
   <router-view></router-view>
-  <div>我是APP</div>
+  <!-- <div>我是APP</div>
   <test-demo></test-demo>
   <el-button @click="$router.push('/home')" type="success">跳首页</el-button>
   <el-button @click="goList">跳列表页</el-button>
   <p>{{ userStore.token }}</p>
   <el-button type="" @click="userStore.setToken('tyh')">登录</el-button>
   <el-button type="" @click="userStore.removeToken()">退出</el-button>
-  <el-button type="" @click="getUser">getUser</el-button>
+  <el-button type="" @click="getUser">getUser</el-button> -->
 </template>
 
 <style scoped></style>
--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,6 +1,121 @@
-<script setup></script>
+<script setup>
+import { User, Lock } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+const isRegister = ref(true)
+</script>
 <template>
-  <div>登录页</div>
+  <!-- el-row表示一行，一行分成24份，分成12+3+6+3 
+        el-col 表示列 
+        （1）:span="12" 表示在一行中占12份
+        （2）:offset="3" 在一行中距离左侧的margin份数
+    el-form 整个表单组件
+    el-form-item 表单的一行（一个表单域）
+    el-input 表单元素
+    -->
+  <el-row class="login-page">
+    <el-col :span="12" class="bg"></el-col>
+    <el-col :span="6" :offset="3" class="form">
+      <!-- 注册相关表单 -->
+      <el-form ref="form" size="large" autocomplete="off" v-if="isRegister">
+        <el-form-item>
+          <h1>注册</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入再次密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>
+            注册
+          </el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = false">
+            ← 返回
+          </el-link>
+        </el-form-item>
+      </el-form>
+      <!-- 登录相关表单 -->
+      <el-form ref="form" size="large" autocomplete="off" v-else>
+        <el-form-item>
+          <h1>登录</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            name="password"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="flex">
+          <div class="flex">
+            <el-checkbox>记住我</el-checkbox>
+            <el-link type="primary" :underline="false">忘记密码？</el-link>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space
+            >登录</el-button
+          >
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = true">
+            注册 →
+          </el-link>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.login-page {
+  height: 100vh;
+  background-color: #fff;
+
+  .bg {
+    background:
+      url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
+      url('@/assets/login_bg.jpg') no-repeat center / cover;
+    border-radius: 0 20px 20px 0;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    user-select: none;
+
+    .title {
+      margin: 0 auto;
+    }
+
+    .button {
+      width: 100%;
+    }
+
+    .flex {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
